cde66253a18457040bae6f9f1b37739cd8862617
Kal <info@kalacademy.org>
2023-03-18 12:19:33 -0700

    Add project files.

---
+++ ToDoApp/ToDoApp/ToDoPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoPage : ContentPage
+    {
+        public ToDoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var todo = new ToDo();
+            todo.Text = ToDoText.Text;
+
+        }
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
